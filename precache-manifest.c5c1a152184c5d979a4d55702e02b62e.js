5c5b05c44c4840be0629a2d96755d8109a2e45fc
Code Sandbox <hello@codesandbox.io>
2022-11-05 16:42:08 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.c5c1a152184c5d979a4d55702e02b62e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03d4150bd34cb8d4aa92e2c5d36e33f",
+    "url": "https://arul2112.github.io/csb-8ck4tm/index.html"
+  },
+  {
+    "revision": "5e9e280f81e60d7880a0",
+    "url": "https://arul2112.github.io/csb-8ck4tm/static/js/2.450492b5.chunk.js"
+  },
+  {
+    "revision": "104344b3590b0702e89f",
+    "url": "https://arul2112.github.io/csb-8ck4tm/static/js/main.9b98977b.chunk.js"
+  },
+  {
+    "revision": "714096c366afa703e1c6",
+    "url": "https://arul2112.github.io/csb-8ck4tm/static/js/runtime-main.7e054d2e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://arul2112.github.io/csb-8ck4tm/precache-manifest.c5c1a152184c5d979a4d55702e02b62e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arul2112.github.io/csb-8ck4tm/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
